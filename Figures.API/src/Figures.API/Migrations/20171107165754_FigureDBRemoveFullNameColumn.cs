a765d6718cbf8c903d150d1d1aba0aca442705e3
István Fazekas <istvan.fazekas@dealogic.com>
2017-11-07 17:58:31 +0100

    create FigureDBRemoveFullNameColumn migration

---
+++ Figures.API/src/Figures.API/Migrations/20171107165754_FigureDBRemoveFullNameColumn.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Figures.API.Migrations
+{
+    public partial class FigureDBRemoveFullNameColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "Figures");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "Figures",
+                maxLength: 152,
+                nullable: true);
+        }
+    }
+}
